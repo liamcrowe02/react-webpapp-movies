6dafd8ed7721f9ba9f61e506f329ce12b7bdc185
LiamCrowe2502 <20093470@mail.wit.ie>
2023-10-25 19:39:12 +0100

    Updating movie page to use react query

--- movies/src/components/filterMoviesCard/index.js
+++ movies/src/components/filterMoviesCard/index.js
@@ -49,17 +49,33 @@ export default function FilterMoviesCard(props) {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [])
 
-  const handleChange = (e, type, value) => {
-    e.preventDefault()
-    props.onUserInput(type, value)   // NEW
+  const { data, error, isLoading, isError } = useQuery("genres", getGenres);
+
+  if (isLoading) {
+    return <Spinner />;
   }
-  const handleTextChange = e => {
-    handleChange(e, "name", e.target.value)
+
+  if (isError) {
+    return <h1>{error.message}</h1>;
   }
-  const handleGenreChange = e => {
-    handleChange(e, "genre", e.target.value)
-  };
   
+  const genres = data.genres;
+  if (genres[0].name !== "All"){
+    genres.unshift({ id: "0", name: "All" });
+  }
+
+  const handleChange = (e, type, value) => {
+    e.preventDefault();
+    props.onUserInput(type, value); // NEW
+  };
+
+  const handleTextChange = (e, props) => {
+    handleChange(e, "name", e.target.value);
+  };
+
+  const handleGenreChange = (e) => {
+    handleChange(e, "genre", e.target.value);
+  };
   return (
     <Card 
       sx={{
